ddb5404a8b00495c737b94dcdbf110c902946a58
Rutsell Moradel <104604605+Rutsell64@users.noreply.github.com>
2022-05-13 15:37:16 -0400

    Update log-out.php

--- FinalProject/log-out.php
+++ FinalProject/log-out.php
@@ -5,6 +5,6 @@
 ?>
 
 <h3>Successfully logged out.</h3>
-<h3>Click <a id="link" href="/MGL/home.php">here</a> to go back to the home page.<h3>
+<h3>Click <a id="link" href="home.php">here</a> to go back to the home page.<h3>
 
 <?php include("bottom.html"); ?>
