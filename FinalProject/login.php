ff6ed2ac309b898960a0bb434e9d1fca5e02d657
Rutsell Moradel <104604605+Rutsell64@users.noreply.github.com>
2022-05-13 11:09:12 -0400

    Update login.php

--- FinalProject/login.php
+++ FinalProject/login.php
@@ -1,34 +1,35 @@
-<?php include("top.php"); ?>
+<DOCTYPE html>
+	<head>
+		<link href = "MGL.css" type = "text/css" rel = "stylesheet"/>
+	</head>
+	<body> 
+		<div id="banner">
 
-
-<div class="login">
-
-<form action="/MGL/login-submit.php" method="post"> 
+            <a href="home.php">
+                <img class="logo"src="logo.png" width = "150" height = "150" />
+            </a>
+    </div>
+		<div class = "login_box">
+			<form class = "form" action = "login-submit.php" method = "post">
 		
-		<fieldset>
-
-
+				<fieldset>
+				<!-- This is how the user signup menu is created -->
+			
+					<p class = "new_user_title">Login</p>
 				
-			<input type="text" name="username" placeholder="username" autocomplete="off" required /> <br> <br>
-			<input type="password" name="password" placeholder="password" required /> <br> <br>
+						<h1><input type = "username" name = "username" maxlength = "16" size = "29" placeholder = "Username" required /></br></h1>
+					
+						<h1><input type = "password" name = "password" maxlength = "16" size = "29" placeholder = "Password" required /></br></h1>
+					
+						<p class = "login_have_account" ><a href = "signup.php">Don't have an account? Let's create one</a></br></p>
+					
+						<p class = "login_button"><input type = "submit" value = "Log In" /></p>
+					
+				</fieldset></br>
+			
+			</form>
+		</div>
 
+	</body>
 
-
-			<br>
-			<input type="submit" name="submit" value="Log in" />
-		</fieldset>
-
-		
-
-	</form>
-
-	<p class="dont_have_account">Don't have an account? <a id="link" href="signup.php">Let's create one</a></p>
-
-	
-
-
-
-
-</div>
-
-<?php include("bottom.html"); ?>
+</html>
