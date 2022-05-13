e78dac2e1d28910b38197a0edcd2400ef7f82d5f
Rutsell Moradel <104604605+Rutsell64@users.noreply.github.com>
2022-05-13 11:07:49 -0400

    Update signup.php

--- FinalProject/signup.php
+++ FinalProject/signup.php
@@ -1,48 +1,41 @@
-<?php include("top.php"); ?>
-
-
-<div class="signup">
-
-<form action="/MGL/signup-submit.php" method="post"> 
-		
-		<fieldset>
-			
-			<input type="text" name="username" placeholder="username" autocomplete="off" required /> <br> <br>
-			<input type="email" name="email" placeholder="email" autocomplete="off" required /> <br> <br>
-			<input type="password" name="password" placeholder="password" required /> <br> <br>
-
-
-
-			<br>
-			<input type="submit" name="submit" value="Sign up" />
-		</fieldset>
-
-		
-
-</form>
-
-<p class="already_have_account">Already have an account? Click <a id="link" href="login.php">here</a> to log in</p>
-
+<DOCTYPE html>
+	<head>
+		<link href = "MGL.css" type = "text/css" rel = "stylesheet"/>
+	</head>
 	
+	<body> 
+	<div id="banner">
 
+            <a href="home.php">
+                <img class="logo"src="logo.png" width = "150" height = "150" />
+            </a>
+    </div>
+		<div class = "signup_box">
+			<form class = "form" action = "signup-submit.php" method = "post">
+		
+				<fieldset>
+				<!-- This is how the user signup menu is created -->
+			
+						<p class = "new_user_title">Sign Up </p>
+				
+						<h1><input type = "name" maxlength = "16" size = "29" name = "name" placeholder = "Full Name" required /></br></h1>
+					
+						<h1><input type = "password" name = "password" maxlength = "16" size = "29" placeholder = "Password" required /></br></h1>
 
+						<h1><input type = "text" name = "email" placeholder = "Email Address" maxlength = "29" size = "29" required /></br></h1>
+					
+						<h1><input type = "phone_number" maxlength = "10" size = "29" name = "phone_number" placeholder = "Phone Number" required /> </br></h1>
+		
+						<h1><input type = "username" name = "username" maxlength = "16" size = "29" placeholder = "Username" required /></br></h1>	
+					
+						<p class = "signup_have_account" ><a href = "login.php">Have an account? Click here to login</a></br></p>
+		
+						<input type = "submit" value = "Sign Up" />
+				</fieldset></br>
+			
+			</form>
+		</div>
+	
+	</body>
 
-
-</div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<?php include("bottom.html"); ?>
+</html>
